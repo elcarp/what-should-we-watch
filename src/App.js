a45781085a97732a5b13f35848bef1dc118b5c9d
Lise <lise.carpenter@gmail.com>
2021-09-22 20:15:01 +0700

    resolve issue

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Airtable from 'airtable'
 
 require('dotenv').config()
+
 var Airtable = require('airtable')
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 	process.env.AIRTABLE_BASE_ID
